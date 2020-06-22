b9aa80a164bd77e5ce28107d20486d5c955024f3
BetzalelKenig <59796236+BetzalelKenig@users.noreply.github.com>
2020-06-22 15:00:20 +0300

    bild with ss url

--- docs/main-es2015.js
+++ docs/main-es2015.js
@@ -471,7 +471,7 @@ _angular_platform_browser__WEBPACK_IMPORTED_MODULE_3__["platformBrowser"]().boot
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Shmulik Shotland\Desktop\Giraffes\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Users\stu\Desktop\Giraffes\src\main.ts */"./src/main.ts");
 
 
 /***/ })
--- docs/main-es5.js
+++ docs/main-es5.js
@@ -985,7 +985,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
   /***/
   function _(module, exports, __webpack_require__) {
     module.exports = __webpack_require__(
-    /*! C:\Users\Shmulik Shotland\Desktop\Giraffes\src\main.ts */
+    /*! C:\Users\stu\Desktop\Giraffes\src\main.ts */
     "./src/main.ts");
     /***/
   }
